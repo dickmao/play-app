ddcf0457dc5533f3041a55ef65b2c053d990ef4e
dickmao <rchiang@cs.stonybrook.edu>
2018-08-13 18:28:20 -0400

    access logging [skip ci]

--- app/Filters.scala
+++ app/Filters.scala
@@ -1,6 +1,10 @@
 import play.api.http.DefaultHttpFilters
 import play.filters.csrf.CSRFFilter
 import javax.inject.Inject
+import play.filters.gzip.GzipFilter
 
-class Filters @Inject() (csrfFilter: CSRFFilter)
-  extends DefaultHttpFilters(csrfFilter)
+class Filters @Inject() (
+  gzip: GzipFilter,
+  csrf: CSRFFilter,
+  access: AccessLoggingFilter
+) extends DefaultHttpFilters(gzip, csrf, access)
